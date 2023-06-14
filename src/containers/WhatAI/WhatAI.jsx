414aca736582bafe74bac73bc1584d0160c68cd6
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-06-14 16:00:12 -0500

    folder steup done

---
+++ src/containers/WhatAI/WhatAI.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import './whatAi.css'
+
+function WhatAI() {
+  return (
+    <div>WhatAI</div>
+  )
+}
+
+export default WhatAI
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import './Possibility.css'
+
+function Possibility() {
+  return (
+    <div>Possibility</div>
+  )
+}
+
+export default Possibility
