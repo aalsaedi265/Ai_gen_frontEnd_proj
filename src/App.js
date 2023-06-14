85d6f4f2b12fd002ee7f0a4792266d3b3010b95b
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-06-14 16:35:24 -0500

    layout done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+
 import React from 'react';
 import { CTA, Brand, Navbar } from './components';
+import {Footer, Blog, Features, Possibility, WhatAI, Header} from './containers';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+        <div className='gradient_bg'>
+            <Navbar />
+            <Header />
+        </div>
+        <Brand />
+        <WhatAI />
+        <Features />
+        <Possibility/>
+        <CTA />
+        <Blog />
+        <Footer/>
     </div>
   );
 }
