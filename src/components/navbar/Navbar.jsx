db6d3e7809ebdf6b55e7f6045d190bde7a89fc9e
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-06-22 16:52:14 -0500

    got all the assests aka images i need

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+
+import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css'
+
 
 function Navbar
 () {
   return (
-    <div> Navbar
-
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-logo'>
+          <img src={logo} alt='logo'/>
+        </div>
+      </div> 
+      Navbar
     </div>
   )
 }
