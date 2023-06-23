24e26a89f73cc54339de21bc32d9c5399dc98276
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-06-22 20:55:34 -0500

    stoping point for nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,15 +2,23 @@
 import React from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css'
+import logo from '../../assets/GPT-3.svg'
 
 
 function Navbar
 () {
   return (
-    <div className='gpt3__navbar'>
+    <div className='gpt3__navbar '>
       <div className='gpt3__navbar-links'>
-        <div className='gpt3__navbar-logo'>
+        <div className='gpt3__navbar-links_logo '>
           <img src={logo} alt='logo'/>
+        </div>
+        <div className='gpt3__navbar-links_container'>
+          <p><a href='#home'>HOME</a></p>
+          <p><a href='#info'>INFO</a></p>
+          <p><a href='#ai'>AI</a></p>
+          <p><a href='#features'>Case Study</a></p>
+          <p><a href='#lib'>Library</a></p>
         </div>
       </div> 
       Navbar
