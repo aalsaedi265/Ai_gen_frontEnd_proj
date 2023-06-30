37f38eaed78e0dba2695d8be2e6823f56a2c5bb4
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-06-29 22:26:42 -0500

    features is half way done

--- src/containers/WhatAI/WhatAI.jsx
+++ src/containers/WhatAI/WhatAI.jsx
@@ -1,10 +1,30 @@
 
 import React from 'react'
 import './whatAi.css'
+import {Feature} from '../../components/index'
 
 function WhatAI() {
   return (
-    <div>WhatAI</div>
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+
+      <div className="gpt3__whatgpt3-feature">
+        <Feature title="Embrace the Machine?" text =" But what is the AI, and how can you get money with it" />
+      </div>
+
+      <div className="gpt3__whatgpt3-heading">
+         <h1 className='gradient__text'>
+          Go Beyond the limitations of your Average intellect
+         </h1>
+         <p>
+          You don't need limits
+         </p>
+      </div>
+      <div className="gpt3__features-container">
+          <Feature title='Chatbot' text="Just a phase or here to stay"/>
+          <Feature title="Knowledgebase" text="Is it truely meaningful or just a causal dinner conversaitions"/>
+          <Feature title="Education" text="you figure it out on your own"/>
+      </div>
+    </div>
   )
 }
 
--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,20 @@
+
 import React from 'react'
 import './features.css'
 
-function Features() {
+function Features({title, text}) {
   return (
-    <div>Features</div>
+    <div className='gpt3__features-container__feature'>
+      <div className='gpt3__features-container_feature'>
+
+      </div>
+      <h1>
+        {title}
+      </h1>
+      <div className='gpt3__features-container_feature-text'>
+        {text}
+      </div>
+    </div>
   )
 }
 
